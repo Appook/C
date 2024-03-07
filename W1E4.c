60026489b3013e42480a99c819220131aa864418
Appook <pirinen78@gmail.com>
2024-03-07 14:25:34 +0200

    Hööö

---
+++ W1E4.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main()
+{
+    char c = 'S', d = '#';
+    char *p1 = &c;
+    char *p2 = &d;
+    char *p3;
+    printf("Value of c using pointer *p1 = %c \n", *p1);
+    printf("*p2 = %c \n", *p2);
+    p3 = &d;
+    printf("*p3 = %c \n", *p3);
+    p3 = p1;
+    printf("*p3 = %c \n", *p3);
+    printf("*p1 = %c \n", *p1);
+    printf("p3 = %p \n", p3);
+    *p1 = *p2;
+    printf("*p1 = %c \n", *p1);
+    p1 = &c;
+    printf("*p1: %c \n", *p1);
+    printf("Pointer is a variable, which value is the address of the pointed object p1 = %p \n", p1);
+    printf("Address of the pointer: %p \n", &p3);
+    return 0;
+
+}
